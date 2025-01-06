398b653c6f8e6781284eb9479d4103586ff49d4a
Davi Silva Gomes <davisilgo01@gmail.com>
2025-01-06 19:58:46 -0300

    add code quiz

---
+++ src/conponents/Trivia.jsx
@@ -0,0 +1,14 @@
+
+export default function Trivia() {
+    return (
+        <div className="trivia">
+            <div className="question">Qual a capital do Brasil?</div>
+            <div className="answers">
+                <div className="answer ">São Paulo</div>
+                <div className="answer">Palmas</div>
+                <div className="answer">Brasilia</div>
+                <div className="answer">Fortaleza</div>
+            </div>
+        </div>
+    )
+}

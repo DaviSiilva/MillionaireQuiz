63c3fcfb89cdadea72b495e2eaa0c8bbec7cf7e2
Davi Silva Gomes <davisilgo01@gmail.com>
2025-01-07 19:09:56 -0300

    finalização do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
-import "./app.css";
+import {useMemo, useEffect, useState } from "react";
+import Timer from "./conponents/Timer";
 import Trivia from "./conponents/Trivia";
+import Start from "./conponents/Start";
+import "./app.css";
+
 
 function App() {
-  const [questionNumber, setquestionNumber] = useState(1);
+  const [username, setUsername] = useState(null);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("R$ 0");
 
   const data = [
     {
@@ -396,586 +402,59 @@ function App() {
          },
       ],
     },
-    {
-      id: 16,
-      question: "Qual é o papel do protocolo HTTPS?",
-      answers: [
-        { 
-          text: "Melhorar a largura de banda da rede.", 
-          correct: false
-         },
-        { 
-          text: "Gerenciar cookies no navegador.", 
-          correct: false
-         },
-        { 
-          text: "Oferecer uma conexão segura com criptografia.", 
-          correct: true
-         },
-        { 
-          text: "Substituir o HTTP em redes locais.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 17,
-      question: "Qual comando no Linux é usado para listar arquivos em um diretório?",
-      answers: [
-        { 
-          text: "cd", 
-          correct: false
-         },
-        { 
-          text: "ls", 
-          correct: true
-         },
-        { 
-          text: "mkdir", 
-          correct: false
-         },
-        { 
-          text: "rm", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 18,
-      question: "Qual é o principal objetivo do CI/CD?",
-      answers: [
-        { 
-          text: "Criar interfaces de usuário dinâmicas.", 
-          correct: false
-         },
-        { 
-          text: "Automatizar testes de segurança.", 
-          correct: false
-         },
-        { 
-          text: "Automatizar a integração e entrega contínua de software.", 
-          correct: true
-         },
-        { 
-          text: "Configurar contêineres em produção.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 19,
-      question: "Qual das opções abaixo é uma estrutura front-end?",
-      answers: [
-        { 
-          text: "Express", 
-          correct: false
-         },
-        { 
-          text: "React", 
-          correct: true
-         },
-        { 
-          text: "Django", 
-          correct: false
-         },
-        { 
-          text: "Flask", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 20,
-      question: "Qual ferramenta é usada para monitorar logs em aplicações?",
-      answers: [
-        { 
-          text: "Elasticsearch", 
-          correct: true
-         },
-        { 
-          text: "VSCode", 
-          correct: false
-         },
-        { 
-          text: "MongoDB", 
-          correct: false
-         },
-        { 
-          text: "Slack", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 21,
-      question: "Qual das linguagens é usada para estilizar páginas da web?",
-      answers: [
-        { 
-          text: "HTML", 
-          correct: false
-         },
-        { 
-          text: "CSS", 
-          correct: true
-         },
-        { 
-          text: "JavaScript", 
-          correct: false
-         },
-        { 
-          text: "SQL", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 22,
-      question: "Qual é a função do React Native?",
-      answers: [
-        { 
-          text: "Desenvolver aplicações web responsivas.", 
-          correct: false
-         },
-        { 
-          text: "Desenvolver aplicativos móveis usando JavaScript.", 
-          correct: true
-         },
-        { 
-          text: "Gerenciar bancos de dados em tempo real.", 
-          correct: false
-         },
-        { 
-          text: "Criar APIs RESTful.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 23,
-      question: "O que é uma API REST?",
-      answers: [
-        { 
-          text: "Um tipo de banco de dados relacional.", 
-          correct: false
-         },
-        { 
-          text: "Uma interface que permite a comunicação entre sistemas.", 
-          correct: true
-         },
-        { 
-          text: "Um framework front-end para desenvolvimento web.", 
-          correct: false
-         },
-        { 
-          text: "Um protocolo de criptografia de dados.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 24,
-      question: "Qual é a principal vantagem de usar TypeScript no desenvolvimento?",
-      answers: [
-        { 
-          text: "Melhorar o desempenho das aplicações.", 
-          correct: false
-         },
-        { 
-          text: "Adicionar tipagem estática ao JavaScript.", 
-          correct: true
-         },
-        { 
-          text: "Substituir o CSS em projetos modernos.", 
-          correct: false
-         },
-        { 
-          text: "Facilitar a criação de animações em sites.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 25,
-      question: "O que significa a sigla DOM no contexto do desenvolvimento web?",
-      answers: [
-        { 
-          text: "Document Object Model", 
-          correct: true
-         },
-        { 
-          text: "Data Operation Management", 
-          correct: false
-         },
-        { 
-          text: "Dynamic Online Module", 
-          correct: false
-         },
-        { 
-          text: "Document Operation Method", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 26,
-      question: "Qual é a função do Node.js?",
-      answers: [
-        { 
-          text: "Desenvolver interfaces visuais.", 
-          correct: false
-         },
-        { 
-          text: "Criar aplicações web no lado do servidor.", 
-          correct: true
-         },
-        { 
-          text: "Gerenciar estilos CSS em tempo real.", 
-          correct: false
-         },
-        { 
-          text: "Monitorar desempenho de aplicativos móveis.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 27,
-      question: "Qual dessas bibliotecas é usada para gerenciar o estado em aplicações React?",
-      answers: [
-        { 
-          text: "Axios", 
-          correct: false
-         },
-        { 
-          text: "Redux", 
-          correct: true
-         },
-        { 
-          text: "Bootstrap", 
-          correct: false
-         },
-        { 
-          text: "JQuery", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 28,
-      question: "Qual framework é usado para desenvolvimento web com Python?",
-      answers: [
-        { 
-          text: "Angular", 
-          correct: false
-         },
-        { 
-          text: "Laravel", 
-          correct: false
-         },
-        { 
-          text: "Django", 
-          correct: true
-         },
-        { 
-          text: "Vue.js", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 29,
-      question: "Qual é a principal funcionalidade do Firebase?",
-      answers: [
-        { 
-          text: "Monitorar redes de computadores.", 
-          correct: false
-         },
-        { 
-          text: "Fornecer backend como serviço (BaaS).", 
-          correct: true
-         },
-        { 
-          text: "Gerenciar bancos de dados SQL.", 
-          correct: false
-         },
-        { 
-          text: "Criar designs responsivos para a web.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 30,
-      question: "Qual tecnologia é usada para criar aplicativos móveis híbridos?",
-      answers: [
-        { 
-          text: "React Native", 
-          correct: true
-         },
-        { 
-          text: "JQuery", 
-          correct: false
-         },
-        { 
-          text: "HTML5", 
-          correct: false
-         },
-        { 
-          text: "MongoDB", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 31,
-      question: "Qual é o principal objetivo do uso do Bootstrap?",
-      answers: [
-        { 
-          text: "Criar aplicativos back-end escaláveis.", 
-          correct: false
-         },
-        { 
-          text: "Facilitar o desenvolvimento de interfaces responsivas.", 
-          correct: true
-         },
-        { 
-          text: "Gerenciar estados em aplicações React.", 
-          correct: false
-         },
-        { 
-          text: "Realizar consultas a bancos de dados.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 32,
-      question: "Qual das opções é usada para criar componentes em React?",
-      answers: [
-        { 
-          text: "useEffect", 
-          correct: false
-         },
-        { 
-          text: "JSX", 
-          correct: true
-         },
-        { 
-          text: "API Gateway", 
-          correct: false
-         },
-        { 
-          text: "Redux", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 33,
-      question: "O que significa o termo SPA no desenvolvimento web?",
-      answers: [
-        { 
-          text: "Sistema de Páginas Automatizadas", 
-          correct: false
-         },
-        { 
-          text: "Single Page Application", 
-          correct: true
-         },
-        { 
-          text: "Script Programático Avançado", 
-          correct: false
-         },
-        { 
-          text: "Serviço de Processamento Ágil", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 34,
-      question: "Qual desses frameworks é focado em desenvolvimento back-end?",
-      answers: [
-        { 
-          text: "React", 
-          correct: false
-         },
-        { 
-          text: "Vue.js", 
-          correct: false
-         },
-        { 
-          text: "Express.js", 
-          correct: true
-         },
-        { 
-          text: "Tailwind CSS", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 35,
-      question: "Qual linguagem é usada para desenvolvimento nativo no iOS?",
-      answers: [
-        { 
-          text: "Kotlin", 
-          correct: false
-         },
-        { 
-          text: "Swift", 
-          correct: true
-         },
-        { 
-          text: "JavaScript", 
-          correct: false
-         },
-        { 
-          text: "C#", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 36,
-      question: "Qual das opções é uma vantagem do Progressive Web App (PWA)?",
-      answers: [
-        { 
-          text: "Requer instalação na loja de aplicativos.", 
-          correct: false
-         },
-        { 
-          text: "Funciona offline e é acessível pelo navegador.", 
-          correct: true
-         },
-        { 
-          text: "É desenvolvido apenas com linguagens nativas.", 
-          correct: false
-         },
-        { 
-          text: "Não suporta notificações push.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 37,
-      question: "Qual é a função do Webpack no desenvolvimento web?",
-      answers: [
-        { 
-          text: "Criar interfaces de usuário dinâmicas.", 
-          correct: false
-         },
-        { 
-          text: "Empacotar e otimizar arquivos de projetos web.", 
-          correct: true
-         },
-        { 
-          text: "Gerenciar bancos de dados em tempo real.", 
-          correct: false
-         },
-        { 
-          text: "Monitorar logs de servidores.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 38,
-      question: "O que significa JSX no contexto do React?",
-      answers: [
-        { 
-          text: "JavaScript Extended", 
-          correct: false
-         },
-        { 
-          text: "JavaScript XML", 
-          correct: true
-         },
-        { 
-          text: "JavaScript Extra", 
-          correct: false
-         },
-        { 
-          text: "JavaScript Execution", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 39,
-      question: "Qual é a função do pacote Axios em aplicações web?",
-      answers: [
-        { 
-          text: "Estilizar páginas web.", 
-          correct: false
-         },
-        { 
-          text: "Gerenciar chamadas HTTP.", 
-          correct: true
-         },
-        { 
-          text: "Criar componentes dinâmicos.", 
-          correct: false
-         },
-        { 
-          text: "Monitorar logs de aplicação.", 
-          correct: false
-         },
-      ],
-    },
-    {
-      id: 40,
-      question: "Qual é a principal vantagem do uso do Tailwind CSS?",
-      answers: [
-        { 
-          text: "Oferecer classes utilitárias para estilização rápida.", 
-          correct: true
-         },
-        { 
-          text: "Substituir frameworks como React.", 
-          correct: false
-         },
-        { 
-          text: "Criar bancos de dados responsivos.", 
-          correct: false
-         },
-        { 
-          text: "Automatizar chamadas HTTP.", 
-          correct: false
-         },
-      ],
-    },
   ];
 
-  const moneyPyramid = [
-    {id: 1, amount:"R$ 100"},
-    {id: 2, amount:"R$ 200"},
-    {id: 3, amount:"R$ 300"},
-    {id: 4, amount:"R$ 500"},
-    {id: 5, amount:"R$ 1000"},
-    {id: 6, amount:"R$ 2000"},
-    {id: 7, amount:"R$ 4000"},
-    {id: 8, amount:"R$ 8000"},
-    {id: 9, amount:"R$ 16000"},
-    {id: 10, amount:"R$ 32000"},
-    {id: 11, amount:"R$ 64000"},
-    {id: 12, amount:"R$ 125000"},
-    {id: 13, amount:"R$ 250000"},
-    {id: 14, amount:"R$ 500000"},
-    {id: 15, amount:"R$ 1000000"},
-  ].reverse();
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "R$ 100" },
+        { id: 2, amount: "R$ 200" },
+        { id: 3, amount: "R$ 300" },
+        { id: 4, amount: "R$ 500" },
+        { id: 5, amount: "R$ 1.000" },
+        { id: 6, amount: "R$ 2.000" },
+        { id: 7, amount: "R$ 4.000" },
+        { id: 8, amount: "R$ 8.000" },
+        { id: 9, amount: "R$ 16.000" },
+        { id: 10, amount: "R$ 32.000" },
+        { id: 11, amount: "R$ 64.000" },
+        { id: 12, amount: "R$ 125.000" },
+        { id: 13, amount: "R$ 250.000" },
+        { id: 14, amount: "R$ 500.000" },
+        { id: 15, amount: "R$ 1.000.000" },
+      ].reverse(),
+    []
+  );
+
+    useEffect(() => {
+      questionNumber > 1 &&
+        setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+    }, [questionNumber, moneyPyramid]);
 
   return (
     <div className="app">
-      <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
+      {username ? (
+        <>
+        <div className="main">
+        {stop ? 
+        (<h1 className="endText">você ganhou: {earned} </h1>
+        ) : (
+          <>
+          <div className="top">
+          <div className="timer">
+            <Timer setStop={setStop} questionNumber={questionNumber}/>
+          </div>
         </div>
         <div className="bottom">
-          <Trivia/>
+          <Trivia 
+          data={data} 
+          setStop={setStop} 
+          questionNumber={questionNumber}
+          setQuestionNumber={setQuestionNumber} />
         </div>
-      </div>
+        </>
+      )}
+    </div>
       <div className="pyramid">
         <ul className="moneyList">
         {moneyPyramid.map((m) =>(
@@ -986,6 +465,9 @@ function App() {
         ))}
         </ul>
       </div>
+        </>
+      ) : (<Start setUsername={setUsername}/>
+      )};
     </div>
   );
 }
--- src/conponents/Trivia.jsx
+++ src/conponents/Trivia.jsx
@@ -1,13 +1,75 @@
+import { useState, useEffect } from "react";
+import useSound from "use-sound";
+import play from "../assets/play.mp3";
+import correct from "../assets/correct.mp3"
+import wait from "../assets/wait.mp3";
+import wrong from "../assets/wrong.mp3";
 
-export default function Trivia() {
+
+    export default function Trivia({
+        setStop, 
+        data, 
+        questionNumber, 
+        setQuestionNumber
+    }) {
+
+        const [question, setQuestion] = useState(null);
+        const [selectedAnswer, setSelectedAnswer] = useState(null);
+        const [className, setClassName] = useState("answer");
+
+        const [letsPlay] = useSound(play);
+        const [correctAnswer] = useSound(correct);
+        const [wrongAnswer] = useSound(wrong);
+
+        useEffect(() => {
+          letsPlay();
+        }, [letsPlay]);
+
+        useEffect(()=> {
+            setQuestion(data[questionNumber - 1 ]);
+        }, [data, questionNumber]);
+
+        const delay = (duration, callback) => {
+            setTimeout(() =>{
+                callback();
+            }, duration)
+        };
+
+        const handleClick = (a)=> {
+            setSelectedAnswer(a);
+            setClassName("answer active");
+            delay(3000, ()=> 
+                setClassName(a.correct ? "answer correct" : "answer wrong")
+            );
+            delay(5000, ()=> {
+                if (a.correct) {
+                    correctAnswer();
+                    delay(1000, () =>{
+                        setQuestionNumber((prev) => prev + 1);
+                        setSelectedAnswer(null)
+                    });
+                }else{
+                    wrongAnswer();
+                    delay(1000, () =>{
+                        setStop(true);
+                    });
+                }
+            });  
+        }
+
+
     return (
         <div className="trivia">
-            <div className="question">Qual a capital do Brasil?</div>
+            <div className="question">{question?.question}</div>
             <div className="answers">
-                <div className="answer ">São Paulo</div>
-                <div className="answer">Palmas</div>
-                <div className="answer">Brasilia</div>
-                <div className="answer">Fortaleza</div>
+                {question?.answers.map((a) => (
+                    <div 
+                    className={selectedAnswer === a ? className : "answer"} 
+                    onClick={() => handleClick(a)}
+                    >
+                    {a.text}
+                    </div>
+                ))}
             </div>
         </div>
     )
